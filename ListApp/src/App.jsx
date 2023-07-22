c9b499c23b4414c364635e73fc4d70c78481c8fc
axolotliterature <andrewhansen.dev@gmail.com>
2023-07-21 17:51:08 -0700

    minimal todo app completed, no styling, minimal structure

--- ListApp/src/App.jsx
+++ ListApp/src/App.jsx
@@ -1,33 +1,76 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), 
+          title: newItem, 
+          completed: false },
+      ]
+    })
+    setNewItem("")
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodos(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <form onSubmit={handleSubmit} className="new-item-form">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input 
+          value={newItem} 
+          onChange={e => setNewItem(e.target.value)} 
+          type="text" 
+          id="item">
+        </input>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button className="button">Add</button>
+    </form>
+    <h1 className="header">Todo List</h1>
+    <ul className="list">
+      {/* Below is Javascript short-circuiting */}
+      {todos.length === 0 && "No Todos"} 
+      {todos.map(todo => {
+        return <li key={todo.id}>
+          <label>
+            <input type="checkbox" checked={todo.completed}
+            onChange={e => toggleTodo(todo.id, e.target.checked)}></input>
+            {todo.title}
+          </label>
+          <button 
+            onClick={() => deleteTodos(todo.id)} 
+            className="button btn-danger"
+            >
+              Delete
+            </button>
+        </li>
+      })}
+    </ul>
     </>
   )
 }
